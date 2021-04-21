4600b71dfb5d1bc283e361d6c997320fbf13f786
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2021-04-21 19:40:08 -0300

    Treated api data and used FlatList to render data on PlantSelect Screen

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, StyleSheet, Text, Image } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
+import userImg from "../assets/luiz.png";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá</Text>
+        <Text style={styles.userName}>Rodrigo</Text>
+      </View>
+      <Image style={styles.image} source={userImg} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
+  },
+});
